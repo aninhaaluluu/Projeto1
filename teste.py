041c2ce44c562c0f7c58c7f3edb2cfa35899d45c
Ana Luiza Senna <analuizasennasenna@gmail.com>
2025-10-06 15:21:07 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -1,2 +1,8 @@
 import streamlit as st
-st.write("Amor ao mundo")
+
+st.title("Meu programa")
+st.write("Alô mundo")
+
+nome = st.text_input("Digite o seu nome:")
+if nome:
+  st.write(nome.upper())
